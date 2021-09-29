3000726cd3fe419e53a2c34b7ba99607d02abbab
Rahulfreecharge <91590439+Rahulfreecharge@users.noreply.github.com>
2021-09-29 16:27:27 +0530

    Add files via upload

---
+++ AccountController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("account")
+public class AccountController {
+@GetMapping("/")
+void getAccounts() {
+System.out.println("called");
+}
+@GetMapping("/{id}")
+void getAccount(@PathVariable Integer id) {
+System.out.println("called"+id);
+}
+@PostMapping
+String postAccount(@RequestBody xyz account) {
+System.out.println("called");
+System.out.println("Name : " + account.getName());
+System.out.println("Account Number: "+account.getAccNo());
+System.out.println("Address : "+account.getAddress());
+System.out.println("Available Balance : "+account.getBalance());
+System.out.println("Date of Creation : " + account.getDate());
+System.out.println("Account Status : "+account.getStatus());
+return "Account details displayed";
+}
+}
